61257083b3805ceb5db80389fc3ed027a1b67fcf
Bumpy_Lumps <bumpsites@gmail.com>
2025-02-25 19:51:04 -0500

    updating otp custom flow for email, hitting snag

--- src/app/custom-flows/sign-in/page.tsx
+++ src/app/custom-flows/sign-in/page.tsx
@@ -3,28 +3,36 @@
 import * as React from 'react'
 import { useSignUp } from '@clerk/nextjs'
 import { useRouter } from 'next/navigation'
+import { ClerkAPIError } from '@clerk/types'
+import { isClerkAPIResponseError } from '@clerk/nextjs/errors'
 
 export default function Page() {
   const { isLoaded, signUp, setActive } = useSignUp()
   const [verifying, setVerifying] = React.useState(false)
-  const [phone, setPhone] = React.useState('')
+  const [email, setEmail] = React.useState('')
   const [code, setCode] = React.useState('')
+  const [errors, setErrors] = React.useState<ClerkAPIError[]>()
+
+
   const router = useRouter()
 
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault()
+
+      // Clear any errors that may have occurred during previous form submission
+      setErrors(undefined)
 
     if (!isLoaded && !signUp) return null
 
     try {
       // Start the sign-up process using the phone number method
       await signUp.create({
-        phoneNumber: phone,
+        emailAddress: email,
       })
 
       // Start the verification - a SMS message will be sent to the
       // number with a one-time code
-      await signUp.preparePhoneNumberVerification()
+      await signUp.prepareEmailAddressVerification()
 
       // Set verifying to true to display second form and capture the OTP code
       setVerifying(true)
@@ -42,7 +50,7 @@ export default function Page() {
 
     try {
       // Use the code provided by the user and attempt verification
-      const signUpAttempt = await signUp.attemptPhoneNumberVerification({
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({
         code,
       })
 
@@ -58,16 +66,15 @@ export default function Page() {
         console.error(signUpAttempt)
       }
     } catch (err) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      console.error('Error:', JSON.stringify(err, null, 2))
-    }
+        if (isClerkAPIResponseError(err)) setErrors(err.errors)
+        console.error(JSON.stringify(err, null, 2))
+      }
   }
 
   if (verifying) {
     return (
       <>
-        <h1>Verify your phone number</h1>
+        <h1>Verify email address</h1>
         <form onSubmit={handleVerification}>
           <label htmlFor="code">Enter your verification code</label>
           <input value={code} id="code" name="code" onChange={(e) => setCode(e.target.value)} />
@@ -81,16 +88,23 @@ export default function Page() {
     <>
       <h1>Sign up</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="phone">Enter phone number</label>
+        <label htmlFor="phone">Enter email address</label>
         <input
-          value={phone}
-          id="phone"
-          name="phone"
-          type="tel"
-          onChange={(e) => setPhone(e.target.value)}
+          value={email}
+          id="email"
+          name="email"
+          type="string"
+          onChange={(e) => setEmail(e.target.value)}
         />
         <button type="submit">Sign Up</button>
       </form>
+      {errors && (
+        <ul>
+          {errors.map((el, index) => (
+            <li key={index}>{el.longMessage}</li>
+          ))}
+        </ul>
+      )}
     </>
   )
 }
