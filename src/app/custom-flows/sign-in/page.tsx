b41ed4a9b550b3e1b2bc4672316c19a5f0fa80fb
Bumpy_Lumps <bumpsites@gmail.com>
2025-02-25 20:02:55 -0500

    set up custom oauth flow and added github to oauth options

--- src/app/custom-flows/sign-in/page.tsx
+++ src/app/custom-flows/sign-in/page.tsx
@@ -1,110 +1,38 @@
 'use client'
 
 import * as React from 'react'
-import { useSignUp } from '@clerk/nextjs'
-import { useRouter } from 'next/navigation'
-import { ClerkAPIError } from '@clerk/types'
-import { isClerkAPIResponseError } from '@clerk/nextjs/errors'
+import { OAuthStrategy } from '@clerk/types'
+import { useSignIn } from '@clerk/nextjs'
 
-export default function Page() {
-  const { isLoaded, signUp, setActive } = useSignUp()
-  const [verifying, setVerifying] = React.useState(false)
-  const [email, setEmail] = React.useState('')
-  const [code, setCode] = React.useState('')
-  const [errors, setErrors] = React.useState<ClerkAPIError[]>()
+export default function OauthSignIn() {
+  const { signIn } = useSignIn()
 
+  if (!signIn) return null
 
-  const router = useRouter()
-
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault()
-
-      // Clear any errors that may have occurred during previous form submission
-      setErrors(undefined)
-
-    if (!isLoaded && !signUp) return null
-
-    try {
-      // Start the sign-up process using the phone number method
-      await signUp.create({
-        emailAddress: email,
+  const signInWith = (strategy: OAuthStrategy) => {
+    return signIn
+      .authenticateWithRedirect({
+        strategy,
+        redirectUrl: '/custom-flows/sso-callback',
+        redirectUrlComplete: '/',
       })
-
-      // Start the verification - a SMS message will be sent to the
-      // number with a one-time code
-      await signUp.prepareEmailAddressVerification()
-
-      // Set verifying to true to display second form and capture the OTP code
-      setVerifying(true)
-    } catch (err) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      console.error('Error:', JSON.stringify(err, null, 2))
-    }
-  }
-
-  async function handleVerification(e: React.FormEvent) {
-    e.preventDefault()
-
-    if (!isLoaded && !signUp) return null
-
-    try {
-      // Use the code provided by the user and attempt verification
-      const signUpAttempt = await signUp.attemptEmailAddressVerification({
-        code,
+      .then((res) => {
+        console.log(res)
       })
-
-      // If verification was completed, set the session to active
-      // and redirect the user
-      if (signUpAttempt.status === 'complete') {
-        await setActive({ session: signUpAttempt.createdSessionId })
-
-        router.push('/')
-      } else {
-        // If the status is not complete, check why. User may need to
-        // complete further steps.
-        console.error(signUpAttempt)
-      }
-    } catch (err) {
-        if (isClerkAPIResponseError(err)) setErrors(err.errors)
-        console.error(JSON.stringify(err, null, 2))
-      }
+      .catch((err: any) => {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        console.log(err.errors)
+        console.error(err, null, 2)
+      })
   }
 
-  if (verifying) {
-    return (
-      <>
-        <h1>Verify email address</h1>
-        <form onSubmit={handleVerification}>
-          <label htmlFor="code">Enter your verification code</label>
-          <input value={code} id="code" name="code" onChange={(e) => setCode(e.target.value)} />
-          <button type="submit">Verify</button>
-        </form>
-      </>
-    )
-  }
-
+  // Render a button for each supported OAuth provider
+  // you want to add to your app. This example uses only Google.
   return (
-    <>
-      <h1>Sign up</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="phone">Enter email address</label>
-        <input
-          value={email}
-          id="email"
-          name="email"
-          type="string"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button type="submit">Sign Up</button>
-      </form>
-      {errors && (
-        <ul>
-          {errors.map((el, index) => (
-            <li key={index}>{el.longMessage}</li>
-          ))}
-        </ul>
-      )}
-    </>
+    <div>
+      <button onClick={() => signInWith('oauth_google')}>Sign in with Google</button>
+      <button onClick={() => signInWith('oauth_github')}>Sign in with Github</button>
+    </div>
   )
 }
---
+++ src/app/custom-flows/sso-callback/page.tsx
@@ -0,0 +1,11 @@
+
+//rename from rafc 
+const page = () => {
+  return (
+    <div>
+      It's working!
+    </div>
+  )
+}
+
+export default page
