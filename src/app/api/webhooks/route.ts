bcf5db471195843a0ee92874501928220d9944c3
Bumpy_Lumps <bumpsites@gmail.com>
2025-02-28 11:40:18 -0500

    updated esLint + svix code blocks for webhook

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,23 +1,23 @@
 
 
 import { Webhook } from 'svix'
-import { headers } from 'next/headers' // out of date?
-import { WebhookEvent } from '@clerk/nextjs/server'
+
+
+// You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+const webhookSecret: string = process.env.WEBHOOK_SECRET || "your-secret"
 
 export async function POST(req: Request) {
 
-	// You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-	const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
-
-	if (!WEBHOOK_SECRET) {
+	if (!webhookSecret) {
 		throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
 	}
 
 	// Get the headers
-	const headerPayload = headers();
-	const svix_id = headerPayload.get("svix-id");
-	const svix_timestamp = headerPayload.get("svix-timestamp");
-	const svix_signature = headerPayload.get("svix-signature");
+	const svix_id = req.headers.get("svix-id") ?? ""; 
+	const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
+	const svix_signature = req.headers.get("svix-signature") ?? "";
+
+
 
 	// If there are no headers, error out
 	if (!svix_id || !svix_timestamp || !svix_signature) {
@@ -26,22 +26,22 @@ export async function POST(req: Request) {
 		})
 	}
 
-	// Get the body
-	const payload = await req.json()
-	const body = JSON.stringify(payload);
+	//get the body
+	const body = await req.text();
 
-	// Create a new SVIX instance with your secret.
-	const wh = new Webhook(WEBHOOK_SECRET);
+	const sivx = new Webhook(webhookSecret);
 
-	let evt: WebhookEvent
+	let msg: any;
 
 	// Verify the payload with the headers
 	try {
-		evt = wh.verify(body, {
+		msg = sivx.verify(body, {
 			"svix-id": svix_id,
 			"svix-timestamp": svix_timestamp,
 			"svix-signature": svix_signature,
-		}) as WebhookEvent
+
+		});
+		
 	} catch (err) {
 		console.error('Error verifying webhook:', err);
 		return new Response('Error occured', {
@@ -49,15 +49,12 @@ export async function POST(req: Request) {
 		})
 	}
 
-	// Get the ID and type
-	const { id } = evt.data;
-	const eventType = evt.type;
-
+	
 	//instead of just logging the payload
 	//use details to add user to DB
-	console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-	console.log('Webhook body:', body)
+	console.log(msg.data.email_addresses[0].email_address);
+	
 
-	return new Response('', { status: 201 })
+	return new Response("OK", { status: 200 })
 }
 
